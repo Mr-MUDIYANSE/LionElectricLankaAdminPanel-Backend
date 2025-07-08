9f89e59eda475a05fb598d8ef0a51f0de770a811
Mr.MUDIYANE <kanishka2001.info@gmail.com>
2025-07-08 16:15:45 +0530

    cors allow

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import rootRouter from "./src/router/root/root.router.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use('/api/v1', rootRouter);
 
