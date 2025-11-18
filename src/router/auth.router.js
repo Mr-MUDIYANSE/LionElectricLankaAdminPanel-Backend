d8f740567f5e167f06dc2d9a2ab5c6055b616311
Mr.MUDIYANE <kanishka2001.info@gmail.com>
2025-11-18 11:41:02 +0530

    up

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -2,10 +2,6 @@ import { Router } from "express";
 import { create, login } from "../controller/auth.controller.js";
 import { forgotPassword, resetPassword } from "../controller/forgotPassword.controller.js";
 
-import { PrismaClient } from '@prisma/client';
-import DB from "../db/db.js";
-const prisma = new PrismaClient();
-
 const authRouter = Router();
 
 authRouter.post('/login', login);
@@ -17,20 +13,6 @@ authRouter.get('/test', (req, res) => {
         success: true,
         message: 'Auth API working fine 🚀'
     });
-});
-authRouter.get("/fix1", async (req, res) => {
-  await DB.admin.updateMany({
-    where: { email: "kanishka2001.info@gmail.com.com" },
-    data: { email: "kanishka2001.info@gmail.com" }
-  });
-  res.send("Email fixed");
-});
-authRouter.get("/fix2", async (req, res) => {
-  await DB.admin.updateMany({
-    where: { email: "piumihashani1@gmail.com.com" },
-    data: { email: "piumihashani1@gmail.com" }
-  });
-  res.send("Email fixed");
 });
 
 export default authRouter;
